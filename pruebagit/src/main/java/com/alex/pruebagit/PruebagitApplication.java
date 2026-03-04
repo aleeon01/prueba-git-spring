e905fe1f9064fbf95ed9af5a2f2fb698680f4900
aleeon01 <aleeon.01@gmail.com>
2026-03-04 10:44:24 +0100

    Inicializa el proecto Spring Boot con dependencias base

---
+++ pruebagit/src/main/java/com/alex/pruebagit/PruebagitApplication.java
@@ -0,0 +1,13 @@
+package com.alex.pruebagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebagitApplication.class, args);
+	}
+
+}
---
+++ pruebagit/src/test/java/com/alex/pruebagit/PruebagitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.pruebagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebagitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
